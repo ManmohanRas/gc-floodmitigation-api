b984a2202a40b989a315c1c25161c1a0ad93c5ae
Sai Charan Kodati <saicharan@rightanglesol.com>
2023-06-24 21:48:56 -0400

    Added Authorization for Create Application

--- PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Application/Commands/CreateApplication/CreateApplicationCommandHandler.cs
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Application/Commands/CreateApplication/CreateApplicationCommandHandler.cs
@@ -5,7 +5,7 @@ namespace PresTrust.FloodMitigation.Application.Commands;
 /// <summary>
 /// This class handles the command to update data and build response
 /// </summary>
-public class CreateApplicationCommandHandler : IRequestHandler<CreateApplicationCommand, CreateApplicationCommandViewModel>
+public class CreateApplicationCommandHandler : BaseHandler, IRequestHandler<CreateApplicationCommand, CreateApplicationCommandViewModel>
 {
     private IMapper mapper;
     private readonly IPresTrustUserContext userContext;
@@ -15,7 +15,7 @@ public class CreateApplicationCommandHandler : IRequestHandler<CreateApplication
         IMapper  mapper,
         IPresTrustUserContext userContext,
         IApplicationRepository repoApplication
-        ) 
+        ) : base(repoApplication: repoApplication)
     {
         this.mapper = mapper;
         this.userContext = userContext;
@@ -24,11 +24,20 @@ public class CreateApplicationCommandHandler : IRequestHandler<CreateApplication
 
     public async Task<CreateApplicationCommandViewModel> Handle(CreateApplicationCommand request, CancellationToken cancellationToken)
     {
+        AuthorizationCheck(request);
+
         var reqApplication = mapper.Map<CreateApplicationCommand, FloodApplicationEntity>(request);
-        reqApplication.Status = ApplicationStatusEnum.DOI_DRAFT;
+        reqApplication.Status = ApplicationStatusEnum.DECLARATION_OF_INTENT_DRAFT;
         reqApplication.LastUpdatedBy = userContext.Email;
         reqApplication = await repoApplication.SaveAsync(reqApplication);
+        
         var result = mapper.Map<FloodApplicationEntity, CreateApplicationCommandViewModel>(reqApplication);
         return result;
+    }
+
+    private void AuthorizationCheck(CreateApplicationCommand request)
+    {
+        userContext.DeriveRole(request.AgencyId);
+        IsAuthorizedOperation(userRole: userContext.Role, applicationStatus: ApplicationStatusEnum.NONE, operation: UserPermissionEnum.CREATE_APPLICATION);
     }
 }
