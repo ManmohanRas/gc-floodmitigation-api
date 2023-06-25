c743c457041294de7dbcd70cf5111a5d5af13378
Sai Charan Kodati <saicharan@rightanglesol.com>
2023-06-24 21:49:20 -0400

    Altered DB Seed Files

--- PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/DatabaseScripts/Seed/1002_FLOOD_FloodApplicationStatus_Seed.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM  [Flood].[FloodApplicationStatus];
-
-INSERT INTO [Flood].[FloodApplicationStatus]([Id],[Name]) VALUES (1,'DECLARATION_OF_INTENT_DRAFT');
-GO
--- PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/DatabaseScripts/Seed/1003-FLOOD-ApplicationType.sql
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/DatabaseScripts/Seed/1003-FLOOD-ApplicationType.sql
@@ -1,4 +1,5 @@
-DELETE FROM [Flood].[FloodApplicationType]
+DELETE FROM [Flood].[FloodApplicationType];
+
 INSERT INTO [Flood].[FloodApplicationType] ([Id], [Title])  VALUES (1, 'CORE');
 GO
 INSERT INTO [Flood].[FloodApplicationType] ([Id], [Title])  VALUES (2, 'MATCH');
