6010df7ad64a7d71e8b332032543cf058822e53a
ManmohanRas <manmohan@rightanglesol.com>
2023-06-27 12:37:59 -0400

    [Flood].[FloodCommentType] seed
    [Flood].[FloodCommentType] seed

---
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/DatabaseScripts/Seed/1005_FLOOD_FloodCommentType_Seed.sql
@@ -0,0 +1,8 @@
+DELETE FROM  [Flood].[FloodCommentType];
+
+INSERT INTO [Flood].[FloodCommentType]([Id], [Title]) VALUES (1, 'General Comment');
+GO
+
+INSERT INTO [Flood].[FloodCommentType]([Id], [Title]) VALUES (2, 'Staff Comment');
+GO
+
