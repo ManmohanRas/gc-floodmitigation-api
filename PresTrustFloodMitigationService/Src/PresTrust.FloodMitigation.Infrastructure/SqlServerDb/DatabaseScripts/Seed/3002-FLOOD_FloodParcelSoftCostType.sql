290bfa2303fc54f6ea0f76d25d87806b932de0c0
ManmohanRas <manmohan@rightanglesol.com>
2023-09-01 11:33:11 -0400

    Parcel softcost type seed
    Parcel softcost type seed

---
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/DatabaseScripts/Seed/3002-FLOOD_FloodParcelSoftCostType.sql
@@ -0,0 +1,20 @@
+DELETE FROM  [Flood].[FloodParcelSoftCostType];
+SET IDENTITY_INSERT [Flood].[FloodParcelSoftCostType] ON
+
+INSERT INTO [Flood].[FloodParcelSoftCostType]([Id], [Title]) VALUES (1, 'APPRAISALS');
+GO
+
+INSERT INTO [Flood].[FloodParcelSoftCostType]([Id], [Title]) VALUES (2, 'ENVIRONMENTAL_ANALYSIS');
+GO
+
+INSERT INTO [Flood].[FloodParcelSoftCostType]([Id], [Title]) VALUES (3, 'TITLE_SEARCH_INSURANCE');
+GO
+
+INSERT INTO [Flood].[FloodParcelSoftCostType]([Id], [Title]) VALUES (4, 'SURVEY');
+GO
+
+INSERT INTO [Flood].[FloodParcelSoftCostType]([Id], [Title]) VALUES (5, 'DEMOLITION');
+GO
+
+INSERT INTO [Flood].[FloodParcelSoftCostType]([Id], [Title]) VALUES (6, 'OTHER_SOFT_COSTS');
+GO
