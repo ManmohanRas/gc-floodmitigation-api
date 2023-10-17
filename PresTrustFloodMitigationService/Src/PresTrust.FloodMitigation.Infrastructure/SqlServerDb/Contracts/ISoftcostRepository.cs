4a29dcbc42cabe94aa30da68d3ae2d131d4ad173
ManmohanRas <manmohan@rightanglesol.com>
2023-10-17 13:21:41 -0400

    removed GetSoftcostSumsByTypeId method
    removed GetSoftcostSumsByTypeId method

--- PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/Contracts/ISoftcostRepository.cs
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/Contracts/ISoftcostRepository.cs
@@ -4,6 +4,4 @@ public interface ISoftcostRepository
 {
     Task<IEnumerable<FloodParcelSoftcostEntity>> GetAllSoftcostLineItemsAsync(int applicationId, string pamsPin);
     Task<FloodParcelSoftcostEntity> SaveAsync(FloodParcelSoftcostEntity softcost);
-
-    Task<IEnumerable<Dictionary<string, decimal>>> GetSoftcostSumsByTypeId(int applicationId, string pamsPin);
 }
