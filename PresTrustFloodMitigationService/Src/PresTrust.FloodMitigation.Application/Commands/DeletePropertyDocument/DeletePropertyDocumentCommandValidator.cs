385358984681b58dfe4a761911972e81f7f713e3
VinayAdarshRS <vinay@rightanglesol.com>
2023-11-08 12:48:07 -0500

    issue fix

--- PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Application/Commands/DeletePropertyDocument/DeletePropertyDocumentCommandValidator.cs
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Application/Commands/DeletePropertyDocument/DeletePropertyDocumentCommandValidator.cs
@@ -4,14 +4,8 @@
     {
         public DeletePropertyDocumentCommandValidator()
         {
-            RuleFor(command => command.ApplicationId)
-                 .GreaterThan(0).WithMessage("Not a valid Application Id");
-
             RuleFor(command => command.Id)
                  .GreaterThan(0).WithMessage("Not a valid Document Id");
-
-            RuleFor(command => command.PamsPin)
-                .NotEmpty().WithMessage("Not a valid PamsPin");
         }
 
     }
