bd25499a9f76db6c2f61b55344be0c415886f4b6
MaruthiRAS <113196051+MaruthiRAS@users.noreply.github.com>
2023-11-27 14:58:15 +0530

    Admin details tab issue fixed api
    Admin details tab issue fixed

--- PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Domain/Entities/FloodPropertyAdminDetailsEntity.cs
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Domain/Entities/FloodPropertyAdminDetailsEntity.cs
@@ -5,6 +5,15 @@ public class FloodPropertyAdminDetailsEntity
     public int Id { get; set; }
     public int ApplicationId { get; set; }
     public string? PamsPin { get; set; }
+    public DateTime? MunicipalResolutionDate { get; set; }
+    public string MunicipalResolutionNumber { get; set; }
+    public DateTime? FMCPreliminaryApprovalDate { get; set; }
+    public string FMCPreliminaryNumber { get; set; }
+    public DateTime? BCCPreliminaryApprovalDate { get; set; }
+    public string BCCPreliminaryNumber { get; set; }
+    public DateTime? FundingExpirationDate { get; set; }
+    public DateTime? FirstFundingExpirationDate { get; set; }
+    public DateTime? SecondFundingExpirationDate { get; set; }
     public DateTime? DobDocumentsMissingDate { get; set; }
     public DateTime? FmcFinalApprovalDate { get; set; }
     public string? FmcFinalNumber { get; set; }
