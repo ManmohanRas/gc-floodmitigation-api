f0b22a050375d41bbd6e4b333412f90aa71c3a39
saicharankRAS <saicharan@rightanglesol.com>
2023-12-11 02:02:24 +0530

    Fixed Duplicate Parcel Property Creation

--- PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/SqlCommands/ParcelProperty/CreateParcelPropertySqlCommand.cs
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Infrastructure/SqlServerDb/SqlCommands/ParcelProperty/CreateParcelPropertySqlCommand.cs
@@ -3,57 +3,60 @@
 public class CreateParcelPropertySqlCommand
 {
     private readonly string _sqlCommand =
-                 @" INSERT INTO [Flood].[FloodParcelProperty]
-                    (
-                        [ApplicationId],
-                        [PamsPin],
-                        [Priority],
-                        [ValueEstimate],
-                        [EstimatedPurchasePrice],
-                        [IsPreIrenePropertyOwner],
-                        [BRV],
-                        [NfipPolicyNo],
-                        [SourceOfValueEstimate],
-                        [FirstFloorElevation],
-                        [StructureType],
-                        [FoundationType],
-                        [OccupancyClass],
-                        [PercentageOfDamage],
-                        [HasContaminants],
-                        [IsLowIncomeHousing],
-                        [HasHistoricSignificance],
-                        [IsRentalProperty],
-                        [RentPerMonth],
-                        [NeedSoftCost],
-                        [LastUpdatedBy],
-                        [LastUpdatedOn]
-                    )
-                    VALUES
-                    (
-                        @p_ApplicationId,
-                        @p_PamsPin,
-                        @p_Priority,
-                        @p_ValueEstimate,
-                        @p_EstimatedPurchasePrice,
-                        @p_IsPreIrenePropertyOwner,
-                        @p_BRV,
-                        @p_NfipPolicyNo,
-                        @p_SourceOfValueEstimate,
-                        @p_FirstFloorElevation,
-                        @p_StructureType,
-                        @p_FoundationType,
-                        @p_OccupancyClass,
-                        @p_PercentageOfDamage,
-                        @p_HasContaminants,
-                        @p_IsLowIncomeHousing,
-                        @p_HasHistoricSignificance,
-                        @p_IsRentalProperty,
-                        @p_RentPerMonth,
-                        @p_NeedSoftCost,
-                        @p_LastUpdatedBy,
-                        GETDATE()
-                    );
-                    SELECT CAST( SCOPE_IDENTITY() AS INT);";
+                 @" IF NOT EXISTS(SELECT [Id] FROM [Flood].[FloodParcelProperty] WHERE [ApplicationId] = @p_ApplicationId AND [PamsPin] = @p_PamsPin)
+				    BEGIN
+                        INSERT INTO [Flood].[FloodParcelProperty]
+                        (
+                            [ApplicationId],
+                            [PamsPin],
+                            [Priority],
+                            [ValueEstimate],
+                            [EstimatedPurchasePrice],
+                            [IsPreIrenePropertyOwner],
+                            [BRV],
+                            [NfipPolicyNo],
+                            [SourceOfValueEstimate],
+                            [FirstFloorElevation],
+                            [StructureType],
+                            [FoundationType],
+                            [OccupancyClass],
+                            [PercentageOfDamage],
+                            [HasContaminants],
+                            [IsLowIncomeHousing],
+                            [HasHistoricSignificance],
+                            [IsRentalProperty],
+                            [RentPerMonth],
+                            [NeedSoftCost],
+                            [LastUpdatedBy],
+                            [LastUpdatedOn]
+                        )
+                        VALUES
+                        (
+                            @p_ApplicationId,
+                            @p_PamsPin,
+                            @p_Priority,
+                            @p_ValueEstimate,
+                            @p_EstimatedPurchasePrice,
+                            @p_IsPreIrenePropertyOwner,
+                            @p_BRV,
+                            @p_NfipPolicyNo,
+                            @p_SourceOfValueEstimate,
+                            @p_FirstFloorElevation,
+                            @p_StructureType,
+                            @p_FoundationType,
+                            @p_OccupancyClass,
+                            @p_PercentageOfDamage,
+                            @p_HasContaminants,
+                            @p_IsLowIncomeHousing,
+                            @p_HasHistoricSignificance,
+                            @p_IsRentalProperty,
+                            @p_RentPerMonth,
+                            @p_NeedSoftCost,
+                            @p_LastUpdatedBy,
+                            GETDATE()
+                        );
+                        SELECT CAST( SCOPE_IDENTITY() AS INT);
+                    END;";
 
     public CreateParcelPropertySqlCommand()
     {
