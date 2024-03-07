b414afb5d1be35a7dfc315c9166bca7a17fb1d9a
narayanaamara7 <155095387+narayanaamara7@users.noreply.github.com>
2024-03-07 16:18:03 +0530

    open Items

--- PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Application/Queries/GetParcelProperty/GetParcelPropertyQueryHandler.cs
+++ PresTrustFloodMitigationService/Src/PresTrust.FloodMitigation.Application/Queries/GetParcelProperty/GetParcelPropertyQueryHandler.cs
@@ -4,26 +4,39 @@ public class GetParcelPropertyQueryHandler : BaseHandler, IRequestHandler<GetPar
 {
     private IMapper mapper;
     private readonly IApplicationRepository repoApplication;
-    private IParcelPropertyRepository repoProperty;
+    private IParcelPropertyRepository repoParcel;
+    private IParcelRepository repoProperty;
 
     public GetParcelPropertyQueryHandler(
         IMapper mapper,
         IApplicationRepository repoApplication,
-        IParcelPropertyRepository repoProperty
+        IParcelPropertyRepository repoParcel,
+        IParcelRepository repoProperty
        ) : base(repoApplication: repoApplication)
     {
         this.mapper = mapper;
         this.repoApplication = repoApplication;
+        this.repoParcel = repoParcel;
         this.repoProperty = repoProperty;
     }
     public async Task<GetParcelPropertyQueryViewModel> Handle(GetParcelPropertyQuery request, CancellationToken cancellationToken)
     {
         // get application details
         var application = await GetIfApplicationExists(request.ApplicationId);
-
-        // get tech details
-        var Property = await repoProperty.GetAsync(request.ApplicationId, request.PamsPin);
-        var result = mapper.Map<FloodParcelPropertyEntity, GetParcelPropertyQueryViewModel>(Property);
+        // get parcel details
+        var parcel = await repoParcel.GetAsync(request.ApplicationId, request.PamsPin);
+        // get recently locked details if there is no data
+        if (!(parcel.Id > 0))
+        {
+            var property = await repoProperty.GetParcelAsync(application.Id, request.PamsPin);
+            if(property?.LockedAnotherApplicationId > 0)
+            {
+                parcel = await repoParcel.GetAsync(property.LockedAnotherApplicationId.Value, request.PamsPin);
+                parcel.Id = 0;
+                parcel.ApplicationId = application.Id;
+            }
+        }
+        var result = mapper.Map<FloodParcelPropertyEntity, GetParcelPropertyQueryViewModel>(parcel);
 
         return result;
     }
